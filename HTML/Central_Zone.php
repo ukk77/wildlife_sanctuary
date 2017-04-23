af1ce6a206f956d7a34aab40c353636909573d9f
niharkarle <niharkarle@gmail.com>
2017-04-24 01:15:20 +0530

    All files with Zone data are added

---
+++ HTML/Central_Zone.php
@@ -0,0 +1,116 @@
+<html>
+<head>
+<style>
+.image {
+  height: 300px;
+  width: 300px;
+  }
+.footer {
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  padding: 1rem;
+  background-color: #efefef;
+  text-align: center;
+}
+.division {
+	background-color: green;
+    width: 250px;
+    border: 10px solid green;
+	float: right;
+	height: 450px;
+}
+.login {
+float: right;
+}
+.form {
+padding-top: 50px;
+}
+.box {
+    width: 250px;
+	height: 400px;
+    border: 5px solid green;
+    padding: 5px;
+	float:right;
+}
+</style>
+<title>Central Zone</title>
+</head>
+<body>
+ <?php
+ $servername = "127.1.0.0";
+$username = "root";
+$password = "";
+$dbname = "seproj";
+// Create connection
+$con = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($con->connect_error) {
+    die("Connection failed: " . $con->connect_error);
+} 
+   session_start();
+   $u = $_SESSION['login_user'];
+   echo "<p>Welcome $u !</p>";
+ ?>
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.light_green-green.min.css">
+<script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+<!-- Always shows a header, even in smaller screens. -->
+<div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+  <header class="mdl-layout__header">
+    <div class="mdl-layout__header-row">
+      <!-- Title -->
+      <span class="mdl-layout-title">National Parks Of India</span>
+      <!-- Add spacer, to align navigation to the right -->
+      <div class="mdl-layout-spacer"> 
+<form action="#" class ="login">
+  <div class="mdl-textfield mdl-js-textfield mdl-textfield--expandable">
+    <label class="mdl-button mdl-js-button mdl-button--icon" for="sample6">
+      <i class="material-icons">search</i>
+    </label>
+    <div class="mdl-textfield__expandable-holder">
+      <input class="mdl-textfield__input" type="text" id="sample6">
+      <label class="mdl-textfield__label" for="sample-expandable">Search</label>
+    </div>
+  </div>
+</form>
+
+	  </div>
+      <!-- Navigation. We hide it in small screens. -->
+      <nav class="mdl-navigation mdl-layout--large-screen-only">
+        <!-- Expandable Textfield -->
+		
+		
+      </nav>
+    </div>
+  </header>
+  <div class="mdl-layout__drawer">
+    <span class="mdl-layout-title">Welcome!</span>
+    <nav class="mdl-navigation">
+      <a class="mdl-navigation__link" href="Home.php">Home</a>
+	  <a class="mdl-navigation__link" href="North_Zone.php">North Zone</a>
+	  <a class="mdl-navigation__link" href="East_Zone.php">East Zone</a>
+	  <a class="mdl-navigation__link" href="West_Zone.php">West Zone</a>
+	  <a class="mdl-navigation__link" href="South_Zone.php">South Zone</a>
+	  <a class="mdl-navigation__link" href="Central_Zone.php">Central Zone</a>
+	  <a class="mdl-navigation__link" href="">Contact Us</a>
+    </nav>
+  </div>
+  <main class="mdl-layout__content">
+    <div class="page-content"><!-- Your content goes here --><br><br><br><CENTER>
+	<img class = "image" src = "http://www.emapsworld.com/images/india-central-zone-map.gif"><br><br>
+	
+	<!-- Raised button -->
+<button class="mdl-button mdl-js-button mdl-button--raised" >
+  <a class="mdl-navigation__link" href="Central_Zone_Parks.php">Book a Trip</a> 
+</button>
+</CENTER>
+	</div>
+<footer class="footer">
+  <p>Posted by: GOVERNMENT OF INDIA</p>
+  <p>Contact information: <a href="mailto:ayush@gmail.com">contact@gmail.com</a>.</p>
+</footer>
+
+</body>
+</html>
